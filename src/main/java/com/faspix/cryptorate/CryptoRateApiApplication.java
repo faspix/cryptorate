575ddab0486407005757b5d3d818763545928325
faspix <babkoum05@gmail.com>
2025-05-11 00:37:48 +0200

    initial commit

---
+++ src/main/java/com/faspix/cryptorate/CryptoRateApiApplication.java
@@ -0,0 +1,13 @@
+package com.faspix.cryptorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoRateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/faspix/cryptorate/CryptoRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faspix.cryptorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
